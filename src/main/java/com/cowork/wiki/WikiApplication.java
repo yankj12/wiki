ce739362b5f7eabe35175b29cd1806239274425d
cc-lady <chenchen_hardwork@163.com>
2018-04-18 12:58:39 +0800

    20180418 只显示一个输入页面，包含一个输入框，点击提交可由springMVC接收到，路径为 http://localhost:8012/content/viewInput

---
+++ src/main/java/com/cowork/wiki/WikiApplication.java
@@ -0,0 +1,16 @@
+package com.cowork.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 启动应用程序
+ * @author CC
+ *
+ */
+@SpringBootApplication
+public class WikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiApplication.class, args);
+	}
+}
