e9c2b1bc1dfc76326a72b6e27c1a36afec27276f
yankj12 <yankj12@163.com>
2018-05-05 23:11:14 +0800

    提供了一个RESTful风格api来查询文章

--- src/main/java/com/cowork/wiki/web/ArticleController.java
+++ src/main/java/com/cowork/wiki/web/ArticleController.java
@@ -1,7 +1,9 @@
 package com.cowork.wiki.web;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,6 +33,35 @@ public class ArticleController {
 	public void inputItem(HttpServletResponse response) throws Exception {
 				
 		response.sendRedirect("/article.html");
+	}
+
+	@RequestMapping(value = "/article/{id}",method = RequestMethod.GET,consumes="application/json")
+	public ResponseVo queryArticle(@PathVariable String id) throws JsonProcessingException {
+		ResponseVo responseVo = new ResponseVo();
+		responseVo.setSuccess(false);
+		responseVo.setErrorMsg(null);
+		
+		if(id != null && !"".equals(id.trim())) {
+			//查询数据
+			List<ServiceInstance> instances = discoveryClient.getInstances("wiki-mongo-data-service");
+			ServiceInstance instance = instances.get(0);
+			
+			URI uri = instance.getUri();
+			String apiUrl = uri.toString() + "/api/queryArticle?id={id}";
+			Map<String, Object> map = new HashMap<>();
+			map.put("id", id);
+			
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseVo responseVo2 = restTemplate.getForObject(apiUrl, ResponseVo.class, map);
+			if(responseVo2 != null && responseVo2.isSuccess() && responseVo2.getArticle() != null) {
+				ArticleVo article = responseVo2.getArticle();
+				
+				responseVo.setSuccess(true);
+				responseVo.setArticle(article);
+			}
+		}
+		
+		return responseVo;
 	}
 	
 	//通过输入页面的submit按钮，可提交输入的内容到这里显示出来
